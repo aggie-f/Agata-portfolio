307e10099ec0ca03793c1fdcaee3ef70d37a7313
Agata Filus <agatafilus@gmail.com>
2021-07-02 16:39:55 +0100

    smooth scroll and hover effect added to project images

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-const sliderContainer = document.querySelector(".slider-container");
-const slideRight = document.querySelector(".right-slide");
-const slideLeft = document.querySelector(".left-slide");
-const upButton = document.querySelector(".up-button");
-const downButton = document.querySelector(".down-button");
-const slidesLength = slideRight.querySelectorAll("div").length;
+window.scroll({
+  top: 0,
+  left: 0,
+  behavior: "smooth",
+});
 
-let activeSlideIndex = 0;
+// const sliderContainer = document.querySelector(".slider-container");
+// const slideRight = document.querySelector(".right-slide");
+// const slideLeft = document.querySelector(".left-slide");
+// const upButton = document.querySelector(".up-button");
+// const downButton = document.querySelector(".down-button");
+// const slidesLength = slideRight.querySelectorAll("div").length;
 
-slideLeft.style.top = `-${(slidesLength - 1) * 70}vh`;
+// let activeSlideIndex = 0;
 
-upButton.addEventListener("click", () => changeSlide("up"));
-downButton.addEventListener("click", () => changeSlide("down"));
+// slideLeft.style.top = `-${(slidesLength - 1) * 70}vh`;
 
-const changeSlide = (direction) => {
-  const sliderHeight = sliderContainer.clientHeight;
-  if (direction === "up") {
-    activeSlideIndex++;
-    if (activeSlideIndex > slidesLength - 1) {
-      activeSlideIndex = 0;
-    }
-  } else if (direction === "down") {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesLength - 1;
-    }
-  }
+// upButton.addEventListener("click", () => changeSlide("up"));
+// downButton.addEventListener("click", () => changeSlide("down"));
 
-  slideRight.style.transform = `translateY(-${
-    activeSlideIndex * sliderHeight
-  }px)`;
-  slideLeft.style.transform = `translateY(${
-    activeSlideIndex * sliderHeight
-  }px)`;
-};
+// const changeSlide = (direction) => {
+//   const sliderHeight = sliderContainer.clientHeight;
+//   if (direction === "up") {
+//     activeSlideIndex++;
+//     if (activeSlideIndex > slidesLength - 1) {
+//       activeSlideIndex = 0;
+//     }
+//   } else if (direction === "down") {
+//     activeSlideIndex--;
+//     if (activeSlideIndex < 0) {
+//       activeSlideIndex = slidesLength - 1;
+//     }
+//   }
+
+//   slideRight.style.transform = `translateY(-${
+//     activeSlideIndex * sliderHeight
+//   }px)`;
+//   slideLeft.style.transform = `translateY(${
+//     activeSlideIndex * sliderHeight
+//   }px)`;
+// };
